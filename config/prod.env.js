ad9b39c945dd974a20eef34770adf92edb0fe928
AhadCove <ahadcove@gmail.com>
2018-09-10 21:33:04 -0400

    Need to add env to cloudmain

---
+++ config/prod.env.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  API_KEY: '"AIzaSyDqOdprC6idVvklBIcx8NO2k4DNi4qQG3A"',
+  CLIENT_ID: '"231931804146-450ql9kfkptj0h6o258a93hp7nemqo30.apps.googleusercontent.com"',
+  CLIENT_SECRET: '"s0T0bPbXjtcP_4IFYZlMXlPw"',
+  HOST: '"https://auto-tube.com"'
+}
